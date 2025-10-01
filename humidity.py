61573b40445db305a0c618ea44cafc294006b04c
pmil <paulius.milasauskas@gmail.com>
2025-10-01 19:08:16 +0100

    files

---
+++ humidity.py
@@ -0,0 +1,32 @@
+from prometheus_client import start_http_server, Gauge
+import Adafruit_DHT
+import time
+
+def get_dht_data():
+    DHT_SENSOR = Adafruit_DHT.DHT22
+    DHT_PIN = 18 
+    dht_data = []
+
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+    if humidity is not None and temperature is not None:
+        # print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        dht_data=[temperature, humidity]
+    else:
+        print("Failed to retrieve data from humidity sensor")
+    return dht_data
+
+def process_request(t):
+    time.sleep(t)
+
+if __name__ == '__main__':
+    adafruit_dht_humidity = Gauge('adafruit_dht_humidity','humidity')
+    adafruit_dht_temperature = Gauge('adafruit_dht_temperature','temperature')
+    start_http_server(8000)
+
+    while True:
+        # Get DHT data
+        dht_data = get_dht_data()
+        adafruit_dht_temperature.set(dht_data[0])
+        adafruit_dht_humidity.set(dht_data[1])
+        process_request(60)
