005be845c40bc3b89101004ddc95482adedcbca6
pmil <paulius.milasauskas@gmail.com>
2025-10-07 22:02:30 +0300

    dht

--- zigbee.sh
+++ zigbee.sh
@@ -1,6 +1,22 @@
+# Install required apps
+sudo apt update
+sudo apt install -y mosquitto mosquitto-clients
+sudo systemctl enable mosquitto
+sudo systemctl start mosquitto
+
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo npm install -g typescript
+
+# Test mosquitto
+mosquitto_sub -v -t '#'
+tsc --version
+
 # Go to home dir and clone repo
 cd ~
 git clone --depth 1 https://github.com/Koenkk/zigbee2mqtt.git
 cd zigbee2mqtt/
-sudo npm install -g pnpm
-pnpm install
+npm install --omit=dev
+
+sudo systemctl start zigbee2mqtt
+sudo systemctl enable zigbee2mqtt
