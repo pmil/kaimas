61acd8cee9f47b03172167505640458e43c65b16
pmil <paulius.milasauskas@gmail.com>
2025-10-24 13:09:25 +0300

    older

--- humidity_old.py
+++ humidity_old.py
@@ -11,7 +11,7 @@ def get_dht_data():
 
     if humidity is not None and temperature is not None:
         # print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        dht_data=[temperature.round(0, 2), humidity.round(0, 2)]
+        dht_data=[temperature, humidity]
     else:
         print("Failed to retrieve data from humidity sensor")
     return dht_data
