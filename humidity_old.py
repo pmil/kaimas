41359f4955eee1c906c905f26601d5e7177fc769
pmil <paulius.milasauskas@gmail.com>
2025-10-24 11:48:31 +0300

    older

--- humidity_old.py
+++ humidity_old.py
@@ -1,13 +1,13 @@
 from prometheus_client import start_http_server, Gauge
-import Adafruit_Python_DHT
+import Adafruit_DHT
 import time
 
 def get_dht_data():
-    DHT_SENSOR = Adafruit_Python_DHT.DHT22
+    DHT_SENSOR = Adafruit_DHT.DHT22
     DHT_PIN = 4 
     dht_data = []
 
-    humidity, temperature = Adafruit_Python_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
         # print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
