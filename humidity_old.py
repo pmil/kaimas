fcf90a0a1fbf53a0879249fbe99f6d20a488aa98
pmil <paulius.milasauskas@gmail.com>
2025-10-24 12:43:53 +0300

    older

--- humidity_old.py
+++ humidity_old.py
@@ -7,7 +7,7 @@ def get_dht_data():
     DHT_PIN = 4 
     dht_data = []
 
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR.round(0, 2), DHT_PIN.round(0, 2))
 
     if humidity is not None and temperature is not None:
         # print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
--- pi-home-install.sh
+++ pi-home-install.sh
@@ -1,1 +1,3 @@
 sudo cp ~/kaimas/systemd/prometheus.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus.service
